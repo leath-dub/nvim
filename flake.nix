f578e99d584145f186509666601f35aea8479fbf
leath-dub <fierceinbattle@gmail.com>
2023-10-30 19:52:58 +0000

    silly me

--- flake.nix
+++ flake.nix
@@ -1,29 +1,7 @@
 {
   description = "My neovim config";
 
-  inputs = {
-    nixpkgs.url = "flake:nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self }: {
+    src = "${./.}";
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
-      packages.default = pkgs.stdenv.mkDerivation {
-        name = "neovim-conf";
-        src = ./.;
-
-        installPhase = ''
-          mkdir -p $out
-          cp -r $src $out
-        '';
-
-        meta = {
-          description = "Derivation storing my neovim configuration";
-        };
-      };
-    });
 }
