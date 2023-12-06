37c47e9544de0cb8fefa4fb0cb8033260918ddd5
leath-dub <fierceinbattle@gmail.com>
2023-12-06 17:04:16 +0000

    swapped which-key with nicer looking mini.clue

--- lua/dev/plugin/autoload/mini.lua
+++ lua/dev/plugin/autoload/mini.lua
@@ -10,6 +10,10 @@ return {{
     local hipatterns = require("mini.hipatterns")
     hipatterns.setup({
       highlighters = {
+        fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+        hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+        todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+        note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
         hex_color = hipatterns.gen_highlighter.hex_color(),
       },
     })
@@ -52,4 +56,42 @@ return {{
       use_as_default_explorer = true,
     }
   }
+}, {
+  "echasnovski/mini.clue",
+  config = function ()
+    local miniclue = require("mini.clue")
+    miniclue.setup({
+      triggers = {
+        -- Leader triggers
+        { mode = 'n', keys = '<Leader>' },
+        { mode = 'x', keys = '<Leader>' },
+
+        -- Built-in completion
+        { mode = 'i', keys = '<C-x>' },
+
+        -- `g` key
+        { mode = 'n', keys = 'g' },
+        { mode = 'x', keys = 'g' },
+
+        -- Marks
+        { mode = 'n', keys = "'" },
+        { mode = 'n', keys = '`' },
+        { mode = 'x', keys = "'" },
+        { mode = 'x', keys = '`' },
+
+        -- Registers
+        { mode = 'n', keys = '"' },
+        { mode = 'x', keys = '"' },
+        { mode = 'i', keys = '<C-r>' },
+        { mode = 'c', keys = '<C-r>' },
+
+        -- Window commands
+        { mode = 'n', keys = '<C-w>' },
+
+        -- `z` key
+        { mode = 'n', keys = 'z' },
+        { mode = 'x', keys = 'z' },
+      },
+    })
+  end,
 }}
