07277158d4c0ad237692742ce122446e89177b36
leath-dub <fierceinbattle@gmail.com>
2023-12-25 17:45:10 +0000

    .

--- lua/dev/plugin/autoload/treesitter.lua
+++ lua/dev/plugin/autoload/treesitter.lua
@@ -8,7 +8,7 @@ return {
       return {}
     end
     nvim_treesitter_config.setup {
-      ensure_installed = { "lua", "vim", "rust", "yuck", "go", "python", "c", "zig", "markdown", "markdown_inline", "dart", "bash", "javascript", "html", "kotlin", "make", "hcl", "svelte", "jsdoc", "css", "todotxt", "csv", "templ" },
+      ensure_installed = { "lua", "vim", "rust", "yuck", "go", "python", "c", "zig", "markdown", "markdown_inline", "dart", "bash", "javascript", "html", "kotlin", "make", "hcl", "svelte", "jsdoc", "css", "todotxt", "csv" },
       sync_install = false,
       highlight = {
         enable = true,
